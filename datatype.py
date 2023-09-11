1253065d87be3ba5ef74341dbfeee72cddc4d7c5
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-12 00:56:49 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,22 @@
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+
+y = str("Hello World")
+
+#display y:
+print(y)
+
+#display the data type of y:
+print(type(y)) 
+
+
+z=5j
+
+print(type(z))
