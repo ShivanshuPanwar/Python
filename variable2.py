255af15a3de8796f3fd50c492b9eb7b373370eff
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-11 22:39:49 +0530

    Add files via upload

---
+++ variable2.py
@@ -0,0 +1,15 @@
+# one value to multiple variables
+
+x = y = z = "Coding"
+
+print(x)
+print(y)
+print(z)
+
+# Many Values to Multiple Variables
+
+l , m , n = "Human","Male","Boy"
+
+print(l)
+print(m)
+print(n)
