02bb01521a208ee1dd2c41d5c04bd0ef3f930544
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-11 22:37:54 +0530

    Add files via upload

---
+++ ifeleif.py
@@ -0,0 +1,8 @@
+x=2
+y=5
+
+if x>y:
+ print("x is greater then y")
+
+elif y>x:      # in another programming language(like C,Java etc.) instead of elif we use else if
+    print("y is greater then x")
