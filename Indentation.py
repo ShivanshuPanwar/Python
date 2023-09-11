99dd3747fc9ebbea9882517926155afb3bb76ae3
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-11 22:38:17 +0530

    Add files via upload

---
+++ Indentation.py
@@ -0,0 +1,18 @@
+"""
+                            Python Indentation
+
+Indentation refers to the spaces at the beginning of a code line.
+
+Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+
+Python uses indentation to indicate a block of code.
+
+"""
+
+
+# Python will give you an error if you skip the indentation:
+
+if 5>3:
+# print("Python will give you an error if you skip the indentation:")
+
+ print("Python will give you an error if you skip the indentation:")
