5eca7ee6832c1f45a1b582e59239a1aff6fbb9ac
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-11 22:38:48 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,20 @@
+# variable in python
+
+"""
+Python has no command for declaring a variable.
+
+A variable is created the moment you first assign a value to it.
+
+Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+"""
+x=5         # x is a variable of int type
+y="hello"   # y is a variable of string type
+
+z='yash'    # String variables can be declared either by using single or double quotes:
+X=7     # Variable names are case-sensitive.
+
+print(type(x))
+print(y)
+print(z)
+print(X)
