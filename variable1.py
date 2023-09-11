d1681871c400b7b0886a8dc34458c10be0c70a95
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-11 22:39:11 +0530

    Add files via upload

---
+++ variable1.py
@@ -0,0 +1,22 @@
+# variable names
+
+myvar = "Shiva"
+my_var = "Shiva"
+_my_var = "Shiva"
+myVar = "Shiva"
+MYVAR = "Shiva"
+myvar2 = "Shiva"
+myVariableName = "Shiva"     # Camel Case
+MyVariableName = "Shiva"      # Pascal Case
+my_variable_name = "Shiva"    # Snake Case
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+print(myVariableName)
+print(MyVariableName)
+print(my_variable_name)
