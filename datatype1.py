097e29957d9ea44413d1c5dec375b54b2903199f
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-12 00:57:08 +0530

    Add files via upload

---
+++ datatype1.py
@@ -0,0 +1,14 @@
+# type conversion by using int(), float(), and complex() methods
+
+x=5
+y=3.0
+z=6j
+
+a = float(x)
+b = int(y) 
+c = complex(x)
+                     # You cannot convert complex numbers into another number type.
+
+print(a)
+print(b)
+print(c)
