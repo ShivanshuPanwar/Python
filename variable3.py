6b8166a3dfaee2cee04eb72a3b98048a31d70129
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-11 22:40:15 +0530

    Add files via upload

---
+++ variable3.py
@@ -0,0 +1,18 @@
+x=7
+y="Mayank"
+
+# print(x + y)     TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+print(x,y)
+
+
+l=6
+m=8
+
+print(l + m)
+
+
+a="Raj "
+b="Raja "
+
+print(a + b)
