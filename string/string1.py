ddda0961db872db5044fb88630186cbe661ea6aa
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-22 00:38:46 +0530

    Add files via upload

---
+++ string/string1.py
@@ -0,0 +1,14 @@
+d = "Python"
+print(len(d))      #  len() function returns the length of a string  # output is 6
+
+
+e = "President of UK"
+print("of" in e)        # output is True
+
+
+f = "President of UK"
+print("is" in f)        # output is false
+
+
+g = "President of UK"
+print("US" not in g)     # ouput is true
