886d3e317fa7124b873bff6f23e6b6addfe733a7
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-22 00:39:14 +0530

    Add files via upload

---
+++ string/string2.py
@@ -0,0 +1,19 @@
+c = "Python"
+print(c[2])        # output is t
+
+
+# You can return a range of characters by using the slice syntax
+
+h = "Basic Python"
+print(h[2:7])        # Get the characters from position 2 to position 7 (not included)  # output is sic P
+
+
+i = "Basic Python"
+print(i[:3])
+
+j = "Basic Python"
+print(j[2:])
+
+
+k = "Basic Python"
+print(k[-5:-2])      # output is yth
