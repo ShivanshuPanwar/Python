21b81c0bacb91a0b0a54bc908c4273220a774581
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-22 00:39:40 +0530

    Add files via upload

---
+++ string/string3.py
@@ -0,0 +1,22 @@
+# modify strings by using built in methods upper(), lower(), strip(), replace(), split()
+
+
+l = "deep coding"
+print(l.upper())
+
+
+m = "DeeP CODInG"
+print(m.lower())
+
+
+n = " Deep  Coding "
+print(n.strip())
+
+
+o = "Deep Coding"
+print(o.replace("Deep","Pro"))
+
+
+p = "deep coding"
+print(p.split())
+
